1ee8dc4c412d855bdfc5b6f66b14f2b579ead0e0
FireSparks87 <125311586+FireSparks87@users.noreply.github.com>
2023-02-13 14:32:44 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,38 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let leftButton = document.querySelector(".go left");
+let rightButton = document.querySelector(".go right");
+//two different first option screen
+let lefttDiv = document.querySelector(".option-one-screen");
+let rightDiv = document.querySelector(".option-two-screen");
+
+//two different end option screens
+let leftEnding = document.querySelector(".go-left-end");
+let rightEnding = document.querySelector(".go-right-end");
+//body
+let body = document.querySelector("body");
+
+
+
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
